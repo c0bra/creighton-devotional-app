ca8d4f49c3436ec536b07905f5a36febc20a4e08
Brian Hann <emailc0bra@gmail.com>
2013-06-07 10:46:40 -0500

    It's working

--- pushGCM.rb
+++ pushGCM.rb
@@ -3,7 +3,7 @@ require 'pushmeup'
 GCM.host = 'https://android.googleapis.com/gcm/send'
 GCM.format = :json
 GCM.key = "AIzaSyATYxVBlq1LXkiPM9ZvYhuwG-IGfrKT7o8"
-destination = ["283590909993"]
+destination = ["APA91bEj2ynaachQ0rue2lwvYs5fkn5iwslh3uZeJey1WHIBs82eA1x1s"]
 data = {:message => "PhoneGap Build rocks!", :msgcnt => "1", :soundname => "beep.wav"}
 
 GCM.send_notification( destination, data)
